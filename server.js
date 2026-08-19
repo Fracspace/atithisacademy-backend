e2aee19200d2ba79805fb94b3d3c91fca7cdb75f
Vishnu teja <vishnuteja476@gmail.com>
2026-08-19 15:01:54 +0530

    made changes to server

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Set DNS resolution to prefer IPv4 (fixes IPv6 ENETUNREACH on platforms like Render)
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 // Load environment variables
 require('dotenv').config();
 
